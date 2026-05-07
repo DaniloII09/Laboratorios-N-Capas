8780f3942e17d234a9502f0ab16dd0fb1b7bce24
DaniloII09 <00377223@uca.edu.sv>
2026-05-07 15:58:42 -0600

    fix: repository annotation

--- laboratorio2/OnePiecePirates/src/main/java/com/example/onepiecepirates/repository/PirateRepository.java
+++ laboratorio2/OnePiecePirates/src/main/java/com/example/onepiecepirates/repository/PirateRepository.java
@@ -2,8 +2,10 @@ package com.example.onepiecepirates.repository;
 
 import com.example.onepiecepirates.model.entity.Pirate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PirateRepository extends JpaRepository<Pirate, UUID> {
 }
