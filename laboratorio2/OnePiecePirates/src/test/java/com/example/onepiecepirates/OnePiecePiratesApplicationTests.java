ff750ecbdf329d7d3b93bf52c35da3f5eef6c388
DaniloII09 <00377223@uca.edu.sv>
2026-05-07 15:37:30 -0600

    add: yaml configuration

---
+++ laboratorio2/OnePiecePirates/src/test/java/com/example/onepiecepirates/OnePiecePiratesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onepiecepirates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePiecePiratesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
